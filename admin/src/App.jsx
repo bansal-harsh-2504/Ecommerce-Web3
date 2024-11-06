5f05da17949e711d0b7a0e591d6377492bf1e0d3
harsh <focusedstudytarget2022@gmail.com>
2024-11-06 18:58:14 +0530

    admin panel login, logout, navbar, sidebar

---
+++ admin/src/App.jsx
@@ -0,0 +1,45 @@
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
+import Login from "./components/Login";
+import { Routes, Route } from "react-router-dom";
+import Add from "./pages/Add";
+import Orders from "./pages/Orders";
+import List from "./pages/List";
+import { useEffect, useState } from "react";
+import { ToastContainer } from "react-toastify";
+
+export const backendUrl = import.meta.env.VITE_BACKEND_URL;
+
+const App = () => {
+  const [token, setToken] = useState(localStorage.getItem("token") || "");
+
+  useEffect(() => {
+    localStorage.setItem("token", token);
+  }, [token]);
+
+  return (
+    <div className="bg-gray-50 min-h-screen">
+      <ToastContainer />
+      {token === "" ? (
+        <Login setToken={setToken} />
+      ) : (
+        <>
+          <Navbar setToken={setToken} />
+          <hr />
+          <div className="flex w-full">
+            <Sidebar />
+            <div className="mx-auto w-[70%] ml-[max(5vw,25px)] my-8 text-gray-600 text-base">
+              <Routes>
+                <Route path="/add" element={Add} />
+                <Route path="/list" element={List} />
+                <Route path="/orders" element={Orders} />
+              </Routes>
+            </div>
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default App;
---
+++ admin/src/components/Login.jsx
@@ -0,0 +1,80 @@
+import { useState } from "react";
+import { backendUrl } from "../App";
+import axios from "axios";
+import { toast } from "react-toastify";
+
+const Login = ({ setToken }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onSubmitHandler = async (e) => {
+    try {
+      e.preventDefault();
+      const res = await axios.post(backendUrl + "/api/user/admin", {
+        email,
+        password,
+      });
+      if (res.data.success) {
+        setToken(res.data.token);
+      } else {
+        toast.error(res.data.message);
+      }
+      toast.success(res.data.message);
+    } catch (error) {
+      toast.error(error.message);
+      console.log("Error while logging in : " + error.message);
+    }
+  };
+
+  return (
+    <div className="min-h-screen flex items-center justify-center w-full">
+      <div className="shadow-md rounded-lg px-8 py-6 max-w-md bg-white">
+        <h1 className="font-bold text-2xl mb-4">Admin Panel</h1>
+        <form onSubmit={onSubmitHandler}>
+          <div className="mb-3 min-w-72">
+            <label
+              className="text-sm font-medium text-gray-700 mb-2"
+              htmlFor="email"
+            >
+              Email Address
+            </label>
+            <input
+              className="rounded-md w-full px-3 py-2 border border-gray-300 outline-none"
+              type="email"
+              placeholder="your@email.com"
+              id="email"
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+              required
+            />
+          </div>
+          <div className="mb-3 min-w-72">
+            <label
+              className="text-sm font-medium text-gray-700 mb-2"
+              htmlFor="password"
+            >
+              Password
+            </label>
+            <input
+              className="rounded-md w-full px-3 py-2 border border-gray-300 outline-none"
+              type="password"
+              placeholder="Enter your password"
+              id="password"
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
+              required
+            />
+          </div>
+          <button
+            className="mt-2 w-full py-2 px-4 rounded-md text-white bg-black"
+            type="submit"
+          >
+            Login
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
