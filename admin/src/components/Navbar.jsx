aa3157e527f88cc7aa3b6edbfee04526190df4dc
harsh <focusedstudytarget2022@gmail.com>
2024-11-25 19:27:48 +0530

    admin: navigate after log out

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -1,11 +1,16 @@
 import { assets } from "../assets/assets";
+import { useNavigate } from "react-router-dom";
 
 const Navbar = ({ setToken }) => {
+  const navigate = useNavigate();
   return (
     <div className="flex items-center py-2 px-[4%] justify-between">
       <img className="w-[max(10%,80px)]" src={assets.logo} alt="" />
       <button
-        onClick={() => setToken("")}
+        onClick={() => {
+          navigate("/");
+          setToken("");
+        }}
         className="bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm"
       >
         Logout
